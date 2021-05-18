483e0a975c1f63a284114be65e391dc72adec236
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-05-18 19:25:08 +0430

    add socket server

--- backend/chat/chat/settings.py
+++ backend/chat/chat/settings.py
@@ -152,3 +152,5 @@ AWS_SERVICE_NAME = 's3'
 AWS_S3_ENDPOINT_URL = 'https://s3.ir-thr-at1.arvanstorage.com'
 AWS_S3_FILE_OVERWRITE = False
 AWS_LOCAL_STORAGE = f'{BASE_DIR}/aws/'
+
+SOCKET_SERVER = config("SOCKET_SERVER")
